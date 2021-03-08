e86540b25cb4f2c94a7583c542dd901ea422623d
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2021-03-07 22:29:42 -0300

    start project with simple model

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Ruby 3
-RSpec 3.10
+RSpec 3.10 / 3.8
---
+++ test_like_a_hero/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  enum kind: [ :knight, :wizard ]
+  validates :level, numericality: { geater_than: 0, less_than_or_equal_to: 99 }
+
+  def title
+    "#{self.kind} #{self.nickname} ##{self.level}"
+  end
+
+end
