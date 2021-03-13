f99e8867471bb65dc472b6fe36d24ce9631ac9d4
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2021-03-13 04:17:13 -0300

    Controller spec request basic examples

--- test_like_a_hero/spec/requests/users_spec.rb
+++ test_like_a_hero/spec/requests/users_spec.rb
@@ -6,5 +6,33 @@ RSpec.describe "Users", type: :request do
       get users_path
       expect(response).to have_http_status(200)
     end
+
+    it "the user's title is present" do
+      users = create_list(:user, 3)
+      get users_path
+      users.each do |user|
+        expect(response.body).to include(user.title)
+      end
+    end
+
   end
+
+  describe "POST /users" do
+    context "when it has valid parameters" do 
+      it "create the user with correct attributes" do
+        user_attributes = FactoryBot.attributes_for(:user)
+        post users_path, params: {user: user_attributes}
+        expect(User.last).to have_attributes(user_attributes)
+      end
+    end
+
+    context "when it has no valid parameters" do
+      it "does not create user" do
+        expect{ 
+          post users_path, params: {user:{ name: '', kind: '', level: '' }}
+        }.to_not change(User, :count)  
+      end
+    end 
+  end
+
 end
