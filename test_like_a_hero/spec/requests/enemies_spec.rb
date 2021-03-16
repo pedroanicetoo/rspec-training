c490597fc89d400b8aedd1cd8fd92e69e129dda9
Pedro Aniceto <pedro.aniceto@cilia.com.br>
2021-03-16 08:31:17 -0300

    Dry some specs

--- test_like_a_hero/spec/requests/enemies_spec.rb
+++ test_like_a_hero/spec/requests/enemies_spec.rb
@@ -3,20 +3,19 @@ require 'rails_helper'
 RSpec.describe "Enemies", type: :request do
   describe "PUT /enemies" do
     context 'when enemy exists' do
+      let(:enemy) { create(:enemy) }
+      let(:enemy_attributes) { @enemy_attributes = attributes_for(:enemy) }
       before :each do
-        @enemy = create(:enemy)
-        @enemy_attributes = attributes_for(:enemy)
-        put "/enemies/#{@enemy.id}", params: @enemy_attributes
+        put "/enemies/#{enemy.id}", params: enemy_attributes
       end
       it "returns status code 200" do
         expect(response).to have_http_status(200)
       end
       it "updates the record" do
-        expect(@enemy.reload).to have_attributes(@enemy_attributes)
+        expect(enemy.reload).to have_attributes(enemy_attributes)
       end
       it "returns the enemy updated" do
-        json_response = JSON.parse(response.body)
-        expect(@enemy.reload).to have_attributes(json_response.except('created_at', 'updated_at'))
+        expect(enemy.reload).to have_attributes(json.except('created_at', 'updated_at'))
       end
     end
 
@@ -35,15 +34,15 @@ RSpec.describe "Enemies", type: :request do
 
   describe "DELETE /enemies" do
     context 'when the enemy exists' do
+      let(:enemy) { create(:enemy) }
       before :each do
-        @enemy = create(:enemy)
-        delete "/enemies/#{@enemy.id}"
+        delete "/enemies/#{enemy.id}"
       end
       it "returns status code 204" do
         expect(response).to have_http_status(204)
       end
       it "destroy the record" do
-        expect{@enemy.reload}.to raise_error ActiveRecord::RecordNotFound
+        expect{enemy.reload}.to raise_error ActiveRecord::RecordNotFound
       end
     end
 
--- test_like_a_hero/spec/requests/weapons_spec.rb
+++ test_like_a_hero/spec/requests/weapons_spec.rb
@@ -4,32 +4,28 @@ RSpec.describe "Weapons", type: :request do
   describe "GET /weapons" do
     before :each do
       @weapons = create_list(:weapon, 3)
+      get weapons_path
     end
     it "works! (now write some real specs)" do
-      get weapons_path
       expect(response).to have_http_status(200)
     end
     context "check if weapon attributes is present" do
       it "the weapon's name is present" do
-        get weapons_path
         @weapons.each do |weapon|
           expect(response.body).to include(weapon.name)
         end
       end
       it "the weapon's current_power is present" do
-        get weapons_path
         @weapons.each do |weapon|
           expect(response.body).to include(weapon.current_power.to_s)
         end
       end
       it "the weapon's title is present" do
-        get weapons_path
         @weapons.each do |weapon|
           expect(response.body).to include(weapon.title)
         end
       end
       it "the weapon link id present" do
-        get weapons_path
         @weapons.each do |weapon|
           href = '/weapons/'+weapon.id.to_s
           expect(response.body).to include(href)
@@ -40,33 +36,33 @@ RSpec.describe "Weapons", type: :request do
 
   describe "GET /weapon" do
     context "Should return weapon attributes" do
+      let(:weapon) { create(:weapon) } 
+      before :each do
+        get weapons_path(weapon.id)
+      end
       weapon = FactoryBot.create(:weapon)
       it "the weapon name is present" do
-        get weapons_path(weapon.id)
         expect(response.body).to include(weapon.name)
       end
-
       it "the weapon current_power is present" do
-        get weapons_path(weapon.id)
         expect(response.body).to include(weapon.current_power.to_s)
       end
-
       it "the weapon title is present" do
-        get weapons_path(weapon.id)
         expect(response.body).to include(weapon.title)
       end
     end
   end
 
   describe "POST /weapons" do
-    weapon_attributes = FactoryBot.attributes_for(:weapon)
+    let(:weapon_attributes) { FactoryBot.attributes_for(:weapon) }
+    before :each do
+      post weapons_path, params:{weapon: weapon_attributes}
+    end
     context "when it has valid parameters" do 
       it "create the weapon with correct attributes" do
-        post weapons_path, params:{weapon: weapon_attributes}
         expect(Weapon.last).to have_attributes(weapon_attributes)
       end
       it "should redirect_to weapons_path" do
-        post weapons_path, params: {weapon: weapon_attributes}
         expect(response).to redirect_to weapons_path
       end
     end
